cd5e2d8e8d6f879efc88f9996470b6624afa3b44
John <jooke98@outlook.com>
2021-02-03 16:35:14 -0700

    map loading and bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import map from "./img/map-v3-big.jpg";
 
 function App() {
   return (
     <div className="App">
-      <h1>Isle Map</h1>
+      <h1>The Isle Map</h1>
+      <img alt="map" src={map} width="60%" ></img>
     </div>
   );
 }
