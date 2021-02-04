cba0c6170c18f9947ddeef88f18597ea13c97926
John <jooke98@outlook.com>
2021-02-04 03:24:01 -0700

    updated tests and css

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-undef */
 /* eslint-disable jest/valid-expect */
+
 describe('Loads page', () => {
   it('Brings up main page', () => {
     cy.visit('localhost:3000');
-  })
-})
+  });
+  it('Loads footer', () => {
+    cy.get('footer').should('exist');
+  });
+});
